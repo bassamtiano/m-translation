7fe6f85f8af346bc79ea5e2085520b9bfa6937e5
bassamtiano <bassamtiano@gmail.com>
2019-06-25 18:55:44 +0900

    adding .gitignore

--- data.py
+++ data.py
@@ -10,44 +10,34 @@ import gzip
 import csv, json, requests, sys
 import nltk
 
+from nltk.corpus import state_union
 from bs4 import BeautifulSoup
 regex_tokenizer = nltk.RegexpTokenizer("\w+")
 
 
-def create_token(text):
-    text = str(text).lower()
-    # text = text.rstrip("\\n")
-    text = text.encode("utf-8", "ignore").decode()
+def create_token(paragraph):
+    paragraph = str(paragraph).lower()
+    paragraph = paragraph.rstrip("\\n")
+    paragraph = paragraph.encode("utf-8", "ignore").decode()
+
+
+
     # remove punktuation symbols
-    text = " ".join(regex_tokenizer.tokenize(text))
-    text = text[1:]
-    text = text[:-2]
-    return text
+    paragraph = " ".join(regex_tokenizer.tokenize(paragraph))
+    paragraph = paragraph[1:]
+    paragraph = paragraph[:-2]
+    return paragraph
 
 def main():
     with open("data/release3.3/data/nucle3.2.sgml") as fp:
         soup = BeautifulSoup(fp, "lxml")
 
-    #
-    # print(soup.prettify())
-    # info = soup.find_all('title')
-
     result = []
-    # for datasets in soup:
-    #     # result.append(soup.find('title'));
-    #     print(soup.find_all('title'))
-
-    # title = [ data.title for data in soup.title ]
-    # print(json.dumps(title, indent=2))
-    # print(title)
-    # print(rs)
 
     article_text = ''
     article = soup.findAll('p')
     for elements in article:
-        # article_text += '\n' + ''.join(elements.findAll(text = True))
         tokenize = create_token(elements.findAll(text = True))
-        # result.append(elements.findAll(text = True))
         result.append(tokenize)
 
     # print(json.dumps(result, indent=4))
